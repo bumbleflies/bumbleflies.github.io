ce3096c5d18e77ede7fa434f50a3cb4e9ac7ed33
dachrisch <christian.daehn@gmail.com>
2022-11-03 16:56:55 +0100

    using parameter for test testing team member

---
+++ _tests/conftest.py
@@ -0,0 +1,60 @@
+import subprocess
+import time
+from http.client import HTTPConnection
+from pathlib import Path
+
+import pytest
+
+
+def http_server_process(directory: str) -> subprocess.Popen:
+    """
+    Call python's http.server module as a child process, wait for initialization and yield server for tests.
+    """
+
+    server = subprocess.Popen(['python', '-m', 'http.server', '--directory', directory],
+                              encoding='utf-8',
+                              universal_newlines=True)
+
+    retries = 5
+    while retries > 0:
+        conn = HTTPConnection('localhost:8000')
+        try:
+            conn.request('HEAD', '/')
+            response = conn.getresponse()
+            if response is not None:
+                return server
+        except ConnectionRefusedError:
+            time.sleep(0.5)
+            retries -= 1
+
+    raise RuntimeError('Failed to start http server')
+
+
+@pytest.fixture(scope='session')
+def path_to_site():
+    return Path(__file__).parent.parent.joinpath('_test_site').absolute()
+
+@pytest.fixture(scope='session')
+def path_to_source():
+    return Path(__file__).parent.parent.absolute()
+
+
+@pytest.fixture(scope='session')
+def bumblebuild(path_to_site: Path, path_to_source:Path):
+    result = subprocess.run(['bundle', 'exec', 'jekyll', 'build', '-d', path_to_site],
+                            capture_output=True, text=True, cwd=path_to_source)
+    assert 'done in' in result.stdout
+
+
+@pytest.fixture(scope='session')
+def bumbleserve(path_to_site, bumblebuild):
+    class Server:
+        def __init__(self):
+            self.host = 'localhost'
+            self.port = 8000
+
+    server_process = http_server_process(path_to_site)
+    yield Server()
+
+    server_process.terminate()
+    server_process.wait()
---
+++ _tests/test_links_and_header.py
@@ -0,0 +1,29 @@
+import pytest as pytest
+
+from _tests.helper import query_site, assert_stylesheet_present, assert_link_present
+
+
+def test_title(bumbleserve):
+    assert 'bumbleflies | 🦋' in query_site(bumbleserve).find(name='title').text
+
+
+@pytest.mark.parametrize('stylesheet', ['bootstrap.min.css', 'all.min.css', 'agency.css', 'custom.css'])
+def test_stylesheets_for_theme_present(bumbleserve, stylesheet):
+    stylesheets = query_site(bumbleserve).find_all(rel='stylesheet')
+    assert_stylesheet_present(stylesheet, stylesheets)
+
+
+@pytest.mark.parametrize('display, url', [('Leistungen', '#Leistungen'),
+                                          ('Über uns', '#about'),
+                                          ('Team', '#Team'),
+                                          ('open:bumble:space', 'obs')])
+def test_navigation(bumbleserve, display, url):
+    nav_links = query_site(bumbleserve).find_all('a', {'class': 'nav-link'})
+    assert_link_present(display, url, nav_links, bumbleserve)
+
+
+@pytest.mark.parametrize('display, url', [('Impressum', 'imprint'),
+                                          ('Datenschutzerklärung', 'privacy')])
+def test_legal(bumbleserve, display, url):
+    legal_links = query_site(bumbleserve).find('ul', {'class': 'quicklinks'}).findChildren('a')
+    assert_link_present(display, url, legal_links, bumbleserve)
