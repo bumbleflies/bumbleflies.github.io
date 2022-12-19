a810eab5f055411760a2fe7dc6780f323f34dbba
dachrisch <christian.daehn@gmail.com>
2022-12-19 13:19:11 +0100

    stayinloop has google form

---
+++ _tests/test_stayintheloop.py
@@ -0,0 +1,11 @@
+from typing import List
+
+import bs4 as bs4
+import pytest as pytest
+from more_itertools import one
+
+from _tests.helper import query_site
+
+def test_stayintheloop_has_google_form(bumbleserve):
+    google_form= one(query_site(bumbleserve, 'stayintheloop').find(id='googleform').findChildren('iframe'))
+    assert google_form['src'].startswith('https://docs.google.com/forms')
