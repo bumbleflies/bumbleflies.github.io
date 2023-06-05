420f94c9f0324e82b71a0ff33ae0142e18ffc03f
dachrisch <christian.daehn@gmail.com>
2023-06-05 12:35:38 +0200

    redirect old pages

--- _tests/test_redirects.py
+++ _tests/test_redirects.py
@@ -1,21 +1,22 @@
 import json
 from os import path
-from bs4 import BeautifulSoup
+
 import pytest as pytest
 
 from _tests.helper import query_site
+
 
 @pytest.fixture()
 def redirects(path_to_site):
     with open(path.join(path_to_site, 'redirects.json')) as redirect_file:
         return json.load(redirect_file)
 
+
 class TestRedirects:
     # https://www.google.com/search?q=+site%3Abumbleflies.de
     @pytest.mark.parametrize('page',
-                         ['imprint', 'privacy', 'stayintheloop', 'obs', 'os-check', 'os-principles'])
+                             ['imprint', 'privacy', 'stayintheloop', 'obs', 'os-check', 'os-principles'])
     def test_page_is_redirecting(self, bumbleserve, page, redirects):
-
         assert redirects[f'/{page}']
 
         assert query_site(bumbleserve, page).find(name='title').text == 'Redirecting...'
