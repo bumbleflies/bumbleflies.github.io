9e82414402ff7bb33d37168995a2b9a2fa39f074
dachrisch <christian.daehn@gmail.com>
2023-06-05 17:35:27 +0200

    purged bootstrap

--- _purge/purge.sh
+++ _purge/purge.sh
@@ -2,4 +2,4 @@
 
 npm i --save-dev purgecss
 
-./node_modules/.bin/purgecss --css ../assets/css/*.css -con ../_site/*.html ../_site/en/*.html -o ../assets/css -s dropdown-toggle dropdown-item show
+./node_modules/.bin/purgecss --css ../assets/css/*.css -con ../_site/*.html ../_site/en/*.html ../_site/assets/js/*.js -o ../assets/css -s dropdown-toggle dropdown-item show
