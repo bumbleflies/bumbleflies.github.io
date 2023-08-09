89de2d8d8278e8a71514fed5fd411e613bb9b9e1
dachrisch <christian.daehn@gmail.com>
2023-08-09 21:39:25 +0200

    prepare FaST Guide

--- _tests/test_sitemap.py
+++ _tests/test_sitemap.py
@@ -1,4 +1,5 @@
 from os import path
+from typing import TextIO
 from urllib.parse import urlparse
 
 import pytest as pytest
@@ -7,18 +8,22 @@ from bs4 import BeautifulSoup
 from _tests.helper import query_site, assert_stylesheet_present
 
 
+def load_locs(sitemap: TextIO):
+    bs_map = BeautifulSoup(sitemap, 'lxml')
+    return filter(lambda l: '/assets' not in l, map(lambda loc: urlparse(loc.text).path,
+                                                    bs_map.find_all('loc')))
+
+
 @pytest.fixture(scope='session')
 def all_pages(path_to_site):
     with open(path.join(path_to_site, 'sitemap.xml')) as sitemap:
-        bs_map = BeautifulSoup(sitemap, 'lxml')
-        return map(lambda loc: urlparse(loc.text).path, bs_map.find_all('loc'))
+        return load_locs(sitemap)
 
 
 @pytest.fixture(scope='session')
 def all_en_pages(path_to_site):
     with open(path.join(path_to_site, 'en', 'sitemap.xml')) as sitemap:
-        bs_map = BeautifulSoup(sitemap, 'lxml')
-        return map(lambda loc: urlparse(loc.text).path, bs_map.find_all('loc'))
+        return load_locs(sitemap)
 
 
 @pytest.mark.parametrize('stylesheet',
@@ -28,10 +33,13 @@ class TestWithSitemap:
 
     def test_stylesheets_for_page_present(self, bumbleserve, all_pages, stylesheet):
         for page in all_pages:
+            print(page)
+            print(page.startswith('/assets'))
             stylesheets = query_site(bumbleserve, page).find_all(rel='stylesheet')
             assert_stylesheet_present(stylesheet, stylesheets)
 
     def test_stylesheets_for_en_page_present(self, bumbleserve, all_en_pages, stylesheet):
         for page in all_en_pages:
+            print(page)
             stylesheets = query_site(bumbleserve, page).find_all(rel='stylesheet')
             assert_stylesheet_present(stylesheet, stylesheets)
