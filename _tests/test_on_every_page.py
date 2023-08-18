f9c1abe7e49fc0f8ee331e7d282cb6093b418c7d
dachrisch <christian.daehn@gmail.com>
2023-08-19 00:31:49 +0200

    new date for obs-4

--- _tests/test_on_every_page.py
+++ _tests/test_on_every_page.py
@@ -5,7 +5,7 @@ from _tests.helper import query_site, assert_link_present, assert_stylesheet_pre
 
 @pytest.mark.parametrize('title, page, allowed_stylesheets', [
     ('Startseite', '/', ()),
-    ('open:bumble:space', '/open-bumble-space', ('https://events.bumbleflies.de/obs-3/widget/v1.css',)),
+    ('open:bumble:space', '/open-bumble-space', ('https://events.bumbleflies.de/obs-4/widget/v1.css',)),
     ('Impressum', '/impressum', ()),
     ('Datenschutzerklärung', '/datenschutz', ()),
     ('Checkliste', '/open-space-checkliste', ()),
