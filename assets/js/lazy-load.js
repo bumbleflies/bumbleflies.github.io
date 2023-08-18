34a4aafa12c3b2b0790dce9bb33026609c5c88de
dachrisch <christian.daehn@gmail.com>
2023-08-18 12:59:52 +0200

    ensure jquery is loaded

--- assets/js/lazy-load.js
+++ assets/js/lazy-load.js
@@ -1,10 +1,17 @@
-$('.modal').on("show.bs.modal", function () {
-    $(this).find('.lazy_load').each(function(){
-        var img = $(this);
-        var attr = img.attr('src')
-        if(typeof attr == 'undefined' || attr == false) {
-            console.log(`lazy loading ${img.data('src')}`)
-            img.attr('src', img.data('src'));
-        }
-    });
-});
+window.onload = function() {
+    if (window.jQuery) {
+        $('.modal').on("show.bs.modal", function () {
+            $(this).find('.lazy_load').each(function(){
+                var img = $(this);
+                var attr = img.attr('src')
+                if(typeof attr == 'undefined' || attr == false) {
+                    console.log(`lazy loading ${img.data('src')}`)
+                    img.attr('src', img.data('src'));
+                }
+            });
+        });
+    } else {
+        // jQuery is not loaded
+        console.log("jQuery not available");
+    }
+}
